a96c1b2dc46a9fa503d618fc195d2567d7bf8172
stein121 <stein121@stein121s-MacBook-Pro.local>
2017-06-17 21:53:15 +0800

    Add azure port code

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('static'));
+var port = process.env.PORT || 3000
 
-var port = process.env.PORT || 3000; 
+app.use(express.static(path.join(__dirname, 'static')));
+// app.use(express.errorHandler());
 
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port + '!');
+
+// Initializing the server when server.js is being executed
+http.createServer(app).listen(port, function() {
+    console.log('Express server listening on port ' + port);
 });
